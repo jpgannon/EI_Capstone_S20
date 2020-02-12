d49f168ed533f283f51b9cb8e208eee90613518c
Robert Coulter <coulterr@vt.edu>
2020-02-12 15:39:54 -0500

    File now loads in all csv files and metadata, cleans data to add NA values, and subtracts pipeheight from levels based on well metadata

--- Cluster_Group/datacleaning.R
+++ Cluster_Group/datacleaning.R
@@ -6,47 +6,42 @@
 library(tidyverse)
 setwd("C:/Capstone/Data/WS3_waterlevel_data")
 
-#create list of well .csv files + w3well_locations.txt
-well_list <- list.files() %>% 
-  as.factor
+##################import well data and well metadata###################3
 
-#read in welldata.txt (must be last file in folder)
-well_locations <- read_csv(well_list[length(well_list)])
+#create list of well .csv files 
+well_list <- list.files() 
 
-#create list of wells 
-wells <- c()
-for (x in 1:nrow(well_locations)){
-  wells[x] <- well_locations$Well[x]
-}
-
-
-#read in well data into a list of dataframes
+#read in well data into a list of tibbles
 well_files <- lapply(well_list, read_csv)
-  
-for (x in 1:length(well_files)-1){
-  wells[x] <- as.data.frame(well_files[x])
+
+#read in well metadata in welldata.txt (located outside of master data folder)
+well_locations <- read_csv("C:/Capstone/Data/w3well_locations.txt")
+
+#create list of wells based on well metadata file
+well_names <- c()
+for (x in 1:nrow(well_locations)){
+  well_names[x] <- well_locations$Well[x]
 }
 
+#name each tibble in the list appropriately
+#each wells data can now be pulled by calling well_files[["well name"]]
+names(well_files) <- well_names
 
+###########loop through each well tibble to clean data#############
 
-#Choose well being visualized
-ID <- "D1"
-#Choose beginning of date range 
-start <- "2011-07-01"
-#Choose end of date range 
-end <- "2011-10-31"
-#Read in well chosen above when creating the ID variable
-well <- read_csv(paste(ID, ".csv", sep = ""))
-#Remove wells with a value of -99
-well$level <-  na_if(well$level, -99)
-#replace the level values above 168 with NA so that they do not show up on the graph 
-well$level <- replace(well$level, well$level > 168, NA)
-#create new variable containing the "w3well_locations" file 
-info <- read_csv("w3well_locations.txt")
-PH <- info$PipeHt[info$Well == ID]
-#Subtract the pipeheight from the level data 
-well$level <- well$level - PH
-#Create a new variable that has the date range selected using the "start" and "end" variables
-welltime <- filter(well, date >= start, date <= end)
+for (x in well_names){
+  #change readings of -99 to NA
+  #well_files[[x]]$level <- na_if(well_files[[x]]$level, -99)
+  well_files[[x]]$level[well_files[[x]]$level == -99] <- NA
+  
+  #change readings greater than 162 (may be 168, must ask JP on this) to NA
+  well_files[[x]]$level[well_files[[x]]$level >= 162] <- NA
+  
+  #subtract pipe height based on well metadata
+  PH <- well_locations$PipeHt[well_locations$Well == x]
+  well_files[[x]]$level <- well_files[[x]]$level - PH
+  
+}
+
 
 
