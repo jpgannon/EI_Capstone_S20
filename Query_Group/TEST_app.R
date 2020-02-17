74849db5a51dd2945ffd7274861f731038f470b7
kmdunlap <your@email.com'g>
2020-02-17 11:55:54 -0500

    the date range selection does not update the plot

--- Query_Group/TEST_app.R
+++ Query_Group/TEST_app.R
@@ -16,9 +16,21 @@ ui <- fluidPage(
   selectInput("select", label = h3("Select Well(s)"), 
               choices = list("1" = 1, "2" = 2, "3" = 3), multiple = TRUE),
   
-  # Date Range
-  dateRangeInput("date", label = h3("Date Range"), start = "2010-08-01", end = "2013-01-10",
-              min = "2010-08-01", max = "2019-10-10"),
+  # Date selection
+      #Select whether user wants single date or range to choose from
+      selectInput('range', "Single Date or Range", 
+                      choices = c("Single Date", "Date Range"),
+                      selected = "Single Date"),
+          
+      #if user chose single date, show a single date input box
+      conditionalPanel(condition = "input.range == 'Single Date'",
+          dateInput("dRange", "Date:", value = "2010-08-01", 
+                    format = "mm/dd/yy")),
+          
+      #if user chose date range, show a date range input box
+      conditionalPanel(condition = "input.range == 'Date Range'",
+          dateRangeInput("dRange", "Date Range:", start = "2010-08-01", end = "2013-01-10",
+                         format = "mm/dd/yy")),
   
   # Stream Flow Button
   checkboxInput("checkbox", label = "View Stream Flow Data", value = FALSE),
@@ -27,8 +39,7 @@ ui <- fluidPage(
   sliderInput("slider", label = h3("Precipitation Range (units)"), min = 0, max = 100, value = c(20, 50)),
   
   # plot and ability to zoom into plot (DO NOT TOUCH plz - katie)
-  fluidRow(
-    column(width = 10, offset = 1, class = "well",
+  fluidRow(column(width = 10, offset = 1, class = "well",
            h4("To zoom: Click and drag box, then double click. Double click plot to zoom back out."),
            plotOutput("wellplot", 
                       dblclick = "plot1_dblclick",
@@ -66,17 +77,18 @@ server <- function(input, output) {
     # filters graph by wells selected
     #wells <- filter(well_data, Well == ID)
     
-    start <- input$date[1]
-    end <- input$date[2]
+    start <- input$dRange[1]
+    end <- input$dRange[2]
     xmin <- input$plot_brush$xmin
     xmax <- input$plot_brush$xmax
+    
     
     ggplot(data = subset, mapping = aes(x = date, y = level))+
       geom_line()+
       scale_y_reverse()+
       ylab("Water Table Depth (cm)")+
+      xlab("Date") + 
       coord_cartesian(xlim = as.POSIXct(ranges$x, origin = "1970-01-01"), expand = FALSE)+
-      scale_y_reverse() +
       theme_classic()
   })
   
@@ -87,6 +99,7 @@ server <- function(input, output) {
                  if (!is.null(brush)) 
                  {
                    ranges$x <- c(brush$xmin, brush$xmax)
+                   #input$dRange <- ranges$x
                    
                  } else {
                    ranges$x <- maxrange$x
