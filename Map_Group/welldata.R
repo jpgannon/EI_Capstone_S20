d6a8c6dd4fa2f78cc15245738678936943506ae6
maoneill97 <maoneill@vt.edu>
2020-02-19 14:45:29 -0500

    start plot code

---
+++ Map_Group/welldata.R
@@ -0,0 +1,13 @@
+library(purrr)
+library(tidyr)
+library(dplyr)
+library(tidyverse)
+library(lubridate)
+library(stringr)
+
+setwd("C:/Users/maone/OneDrive/Documents/SPRING2020/FREC4444/Map_Code/EI_Capstone_S20/Map_Group/")
+
+well_data <- read_csv("well_data.csv")
+
+head(well_data)
+
