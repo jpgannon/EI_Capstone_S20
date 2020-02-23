db21e9b8d4d664ee07d5dcd407a5ecad6aee2304
ltri97 <46967722+ltri97@users.noreply.github.com>
2020-02-22 20:53:53 -0500

    Splitting dataset into smaller ones to plot and compare data availability

--- Cluster_Group/finding_common_dates.R
+++ Cluster_Group/finding_common_dates.R
@@ -29,3 +29,63 @@ ggplot(a_wells,
        aes(x = date,
            y = Well,
            color = has_data)) + geom_point()
+
+#filter more wells
+filter2 <- c("D1", "H4", "I3", "I6", "I7", "I8", "I9")
+wells_set2 <- has_data %>% 
+  filter(Well  %in% filter2)
+
+ggplot(wells_set2,
+       aes(x = date,
+           y = Well,
+           color = has_data)) + geom_point()
+
+filter3 <- c("JD01", "JD02", "JD03", "JD04", "JD05", "JD06", "JD07", "JD08",
+             "JD10", "JD11", "JD12", "JD13", "JD14", "JD15", "JD16", "JD17")
+wells_set3 <- has_data %>% 
+  filter(Well %in% filter3)
+
+ggplot(wells_set3,
+       aes(x = date,
+           y = Well,
+           color = has_data)) + geom_point()
+
+filter4 <- c("JD18","JD19", "JD20", "JD21", "JD22", "JD23", "JD24", "JD25", 
+             "JD26", "JD27", "JD28", "JD29", "JD30")
+wells_set4 <- has_data %>% 
+  filter(Well %in% filter4)
+
+ggplot(wells_set4,
+       aes(x = date,
+           y = Well,
+           color = has_data)) + geom_point()
+
+filter5 <- c("K1", "K1D", "K4D", "K4M", "K4S", "K5", "K6D", "K6S", "K7D", "K7S",
+             "K8", "K9", "K10", "K11", "K12")
+wells_set5 <- has_data %>% 
+  filter(Well %in% filter5)
+
+k1_to_k12 <- ggplot(wells_set5,
+       aes(x = date,
+           y = Well,
+           color = has_data)) + geom_point()
+
+filter6 <- c("N1", "N2", "N3", "N4", "N5", "O1", "O2", "P1", "P2", "Q1", "Q2", "T1")
+wells_set6 <- has_data %>% 
+  filter(Well %in% filter6)
+
+n1_to_t1 <- ggplot(wells_set6,
+       aes(x = date,
+           y = Well,
+           color = has_data)) + 
+  geom_point()
+
+
+#All NAs for h4 and K12??
+h4 <- well_df %>% 
+  filter(Well == "H4")
+k12 <- well_df %>% 
+  filter(Well == "K12")
+
+combo <- k1_to_k12 + n1_to_t1
+combo
