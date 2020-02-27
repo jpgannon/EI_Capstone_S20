8293b2e0ba2397f402c128231be3c1911ad292ac
unknown <ejunk221b@gmail.com>
2020-02-27 14:36:25 -0500

    Still testing functionality but maybe closer?

--- Cluster_Group/Lauren_App_test/app.R
+++ Cluster_Group/Lauren_App_test/app.R
@@ -3,30 +3,11 @@ library(shiny)
 library(tidyverse)
 library(lubridate)
 
-# well_data <- read_csv("C:/Users/Lauren/Documents/Capstone/Data/WS3_waterlevel_data/allwelldata.csv")
-# Welldata_Names <- as.list(unique(well_data$Well))
+Master_List <- read_csv("C:/Users/Lauren/Documents/Capstone/Test_Data/Master_List.csv") #<- this is our local path to that file
 
-#Master_List <- ("C://Master_list_file.csv") #<- this is our local path to that file
-
-Master_List <- data.frame("Well_1" = c("A1", "A1", "A1", "B9", "B9", "B9"), 
-                          "Well_2" = c("B3", "B4", "C9", "A12", "C13", "C4"))
+Well_Names <- as.list(unique(Master_List$Well_1)) #All Well Names
 
 
-
-###This will be deleted when the app is functional
-
-num_wells <- 5 #5 wells
-s_rows <- sample(1:12820210, num_wells) #This will select 5 random rows from within the 12,820,210 rows
-exploratory_sample <- well_data[s_rows,] #This pulls only the 5 randomly selected rows from well_data
-
-sample <- c(exploratory_sample$Well) #This is just to view the 5 randomly selected wells and set to a variable
-
-sample_subset <- well_data %>% #This subsets to only include well data from the 5 random wells
-    filter(Well == sample[1] | Well == sample[2] | Well == sample[3] | Well == sample[4] | Well == sample[5])
-####
-
-dataset <- sample_subset #I would need to have a global file that creates this dataset, or reads it in
-Well_Names <- as.list(unique(sample_subset$Well))
 
 # Define UI for application that plots wells for a specific time period
 ui <- fluidPage(
@@ -37,24 +18,21 @@ ui <- fluidPage(
     # Show a plot of the water level data
         fluidRow(
             column(width = 10, class = "well",
-                   h4("To zoom: Click and drag box, then double click. Double click plot to zoom back out."),
-                   plotOutput("wellplot", 
-                              dblclick = "plot1_dblclick",
-                              brush = brushOpts(
-                                  id = "plot1_brush",
-                                  resetOnNew = TRUE
-                              ))),
-                column(3,
-                       h4("Date Selection"),
+                 plotOutput("wellplot")),
+            
+            column(3,
+                   h4("Date Selection"),
                        dateRangeInput("dates", label = h5("Select the date range for the missing data"))),
-                column(4, offset = 1,
+            
+            column(4, offset = 1,
                        h4("Well Selection"),
                        h5("Well 1 is the well containing missing data that will need to be predicted. Well 2 is the well selected or recommended to be used to predict and fill in Well 1's missing data."),
-                       selectInput("Well_1", label = h5("Select Well 1."), 
+                       selectInput("Well_1_Selection", label = h5("Select Well 1."), 
                                    choices = Well_Names, 
                                    selected = 1),
-                       selectInput('Well_2', label = h5("Select Well 2."),
-                                   choices = Well_Names,
+                   
+                       selectInput('Well_2_Selection', label = h5("Select Well 2."),
+                                   choices = Well_2_Options,
                                    selected = 1),
                 ),
                 column(4,
@@ -69,47 +47,74 @@ server <- function(input, output) {
     start_date <- input$dates[1]
     end_date <- input$dates[2]
     
-    ranges <- reactiveValues(x = as.POSIXct(c(start = "2010-08-01", end = "2013-01-10")))
-    maxrange <- reactiveValues(x = as.POSIXct(c(start = "2010-08-01", end = "2013-01-10")))
+    Well_1 <- input$Well_1_Selection #This is what the user selects for Well 1
+    
+    Well_1_read_file <- paste("C:/Users/Lauren/Documents/Capstone/Test_Data/", Well_1, "_Well.csv", sep = "")
+    Well_1_Data <- read_csv(Well_1_read_file)
+    
+    Well_2_Options <- Master_List %>% #This block of text finds the Well_1 in the Master list and populates the drop down for Well 2 with corresponding Wells
+        filter(Well_1 == input$Well_1_Selection) %>%
+        select(Well_2)
+    
+    Well_2 <- input$Well_2_Selection
+    Well_2_read_file <- paste("C:/Users/Lauren/Documents/Capstone/Test_Data/", Well_2, "_Well.csv", sep = "")
+    Well_2_Data <- read_csv(Well_2_read_file)
+    
+    #for the sake of this test I'm just going to run this as a straight linear regression 
+    #this will probably need to be changed when we do the actual analysis
+    regression <- lm(Well_1_Data$level ~ Well_2_Data$level)
+    
+    #the formula is y = regression$coefficients[2]x + regression$coefficients[1]
+    
+    slope <- regression$coefficients[2]
+    y_int <- regression$coefficients[1]
+    
+    #Create a new column (predicted values) in Well_1 dataset
+    
+    Well_1_Data <- Well_1_Data %>%
+        mutate(predicted_values = NA)
+    
+    #Loop over all the Well_1 original data. If there is an NA, then apply the formula.
+    #Otherwise, keep the original values.
+    num_well1 <- nrow(Well_1_Data)
+    
+    for (i in 1:num_well1) {
+        if (is.na(Well_1_Data$level[i]) == TRUE) {
+            Well_1_Data$predicted_values[i] = ((slope*Well_2_Data$level[i]) + y_int)
+        } else {
+            Well_1_Data$predicted_values[i] = Well_1_Data$level[i]
+        }
+    }
+    
+    Well_1_Data$predicted_values <- as.numeric(Well_1_Data$predicted_values)
+    
+    #Create an additional column "is_predicted" to clearly state whether each value 
+    #in the predicted_values column is either from the original data or predicted and artificial
+    
+    Well_1_Data <- Well_1_Data %>%
+        mutate(is_predicted = NA)
+    
+    for (i in 1:num_well1) {
+        if (is.na(Well_1_Data$level[i]) == TRUE) {
+            Well_1_Data$is_predicted[i] = TRUE
+        } else {
+            Well_1_Data$is_predicted[i] = FALSE
+        }
+    }
+
     
     output$wellplot <- renderPlot({
-        ID <- strsplit(input$Well_1, " ")[[1]]
-        
-        #start <- input$date[1]
-        
-        #end <- input$date[2]
         
         wells <- filter(sample_subset, Well == ID) #, date >= start, date <= end)
         
-        #xmin <- input$plot_brush$xmin
-        
-        #xmax <- input$plot_brush$xmax
-        
-        ggplot(data = wells, mapping = aes(x = date., y = level, color = Well)) +
-            geom_line() +
+        ggplot() +
+            geom_line(aes(Well_1_Data$date, Well_1_Data$predicted_values, color = is_predicted, group = 1), data = Well_1_Data) +
             scale_y_reverse() +
             ylab("Water Table Depth (cm)") +
             xlab("Date") +
-            coord_cartesian(xlim = as.POSIXct(ranges$x, origin = "1970-01-01"), expand = FALSE)+
-            #xlim(ranges$x)+
             theme_classic()
     })
-    
-    # observeEvent(input$plot1_dblclick,
-    observeEvent(input$plot1_dblclick,
-                 {
-                     brush <- input$plot1_brush
-                     if (!is.null(brush)) 
-                     {
-                         ranges$x <- c(brush$xmin, brush$xmax)
-                         
-                     } else {
-                         #ranges$x <- NULL
-                         ranges$x <- maxrange$x
-                         
-                     }
-                 })
-}
+}    
 
 # Run the application 
 shinyApp(ui = ui, server = server)
