0b25ff10cebdbc93653f445de1b2464e91763d40
Robert Coulter <coulterr@vt.edu>
2020-02-28 15:09:56 -0500

    successful DTW test for hourly data included

--- Cluster_Group/ClusterTestRobbie.R
+++ Cluster_Group/ClusterTestRobbie.R
@@ -1,37 +1,35 @@
-#This script creates a list of timeseries of 
-#all of the wells in the JD family that share a common timeframe
-#to test tsclust function
 
 library(tidyverse)
 library(dtwclust)
 
 setwd("C:/Capstone/Data")
 
-sixHourSummary <- read_csv("sixHourSummary.csv")
+data <- read_csv("oneHourSummary.csv")
 
-#For the sake of this test, I will use data from wells 
-#in the JD family, excluding JD03, JD08, JD09, and JD31 for lack of data in the test range
-#Based on sixHourSummary data from datacleaningV2 File
+colnames(data) = c("Well", "Time", "Level", "WtDepth")
 
-start_date <- "2008-01-01"
-end_date <- "2008-04-15"
+start_date <- "2012-01-01"
+end_date <- "2012-06-01"
 
-target <- c("JD01", "JD02", "JD04", "JD05", "JD06", "JD07", "JD10",
-            "JD11", "JD12", "JD13", "JD14", "JD15", "JD16", "JD17", "JD18",
-            "JD19", "JD20", "JD21", "JD22", "JD23", "JD24", "JD25", "JD26",
-            "JD27", "JD28", "JD29", "JD30")
+
+target <- c("T1", "Q2", "Q1", "P2", "O2", "O1", "N5", "N4", "N3", "N2",
+            "N1", "K9", "K8", "K7S", "K7D", "K6S", "K6D", "K5", "K4S", "K4M",
+            "K4D", "K1D", "K11", "K10", "K1", "JD26", "JD25", "JD24", "JD23",
+            "JD22", "JD21", "JD03", "JD02", "I9", "I8", "I7", "I6", "I3", "H4",
+            "D1", "A6")
 
 #filtering test data for tsclust test
-JDWells <- sixHourSummary %>% 
+Wells <- data %>% 
   filter(Well %in% target,
          Time >= start_date,
-         Time <= end_date) 
+         Time <= end_date) %>% 
+  na.omit()
 
 #create list of list for tsclust function
-JDWellsList <- lapply(split(JDWells$WtDepth, JDWells$Well), as.list)
+wellsList <- lapply(split(Wells$WtDepth, Wells$Well), as.list)
 
 #run k-shape clustering algorithms with 5 clusters
-results <- tsclust(JDWellsList, type = "hierarchical", 5, distance = "euclidean")
+results <- tsclust(wellsList, type = "hierarchical", 5, distance = "dtw")
 
 #plot cluster results
 plot(results)
