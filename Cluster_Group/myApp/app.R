521a42d34e3383043f388a7a83aceedada60294e
ltri97 <46967722+ltri97@users.noreply.github.com>
2020-03-31 22:46:36 -0400

    Added download buttons and reduced overall space

--- Cluster_Group/myApp/app.R
+++ Cluster_Group/myApp/app.R
@@ -22,7 +22,23 @@ ui <- fluidPage(
                   choices = wellsList),
       uiOutput("Well_2_Req"),
       uiOutput("date_req"),
-      checkboxInput("Well_2_Plot", "Check to view Well 2 plot data in grey", FALSE)
+      checkboxInput("Well_2_Plot", "Check to view Well 2 plot data in grey", FALSE),
+      downloadButton("downloadplot", "Download Plot!",
+                     style = "background-color:#0dc5c1;
+                      color:#FFFFFF;
+                      border-color:#D1D1D1;
+                      border-style:none;
+                      border-width:1px;
+                      border-radius:5%;
+                      font-size:14px;"),
+      downloadButton("downloaddata", "Download Data!",
+                     style = "background-color:#0dc5c1;
+                      color:#FFFFFF;
+                      border-color:#D1D1D1;
+                      border-style:none;
+                      border-width:1px;
+                      border-radius:5%;
+                      font-size:14px;")
     ),
     
     mainPanel(
@@ -82,66 +98,7 @@ server <- function(input, output){
     
   })
   
-  # Calculate and plot output
-  output$Plot <- renderPlot({
-    
-    Well_1_data <- Well_1_input()
-    Well_2_data <- Well_2_input()
-    
-    Well_1_data <- Well_1_data %>% 
-      filter(date. >= input$dates[1] & date. <= input$dates[2])
-    Well_2_data <- Well_2_data %>% 
-      filter(date. >= input$dates[1] & date. <= input$dates[2])
-    
-    Well_1_data <- Well_1_data %>% 
-      select(date., wtdepth)
-    Well_2_data <- Well_2_data %>% 
-      select(date., wtdepth)
-    
-    combined <- left_join(Well_2_data, Well_1_data, by = "date.")
-    colnames(combined) <- c("date.", "well_2", "well_1")
-    
-    combined <- combined %>% 
-      mutate(is_predicted = ifelse(is.na(well_1), TRUE, FALSE))
-    combined$well_1 <- na.approx(combined$well_1, na.rm = FALSE) #interpolate NAs
-
-    
-    if(input$Well_2_Plot == FALSE){  # only plotting well 1
-      
-      ggplot() +
-        geom_point(data = combined,
-                   mapping = aes(x = date.,
-                                 y = well_1,
-                                 color = is_predicted)) +
-        geom_line(data = combined,
-                  mapping = aes(x = date.,
-                                y = well_1)) +
-        scale_y_reverse() +
-        labs(x = "Date",
-             y = "Water Table Depth (cm)")
-    } else{  # plotting well 1 and well 2
-      
-      ggplot() +
-        geom_point(data = combined,  # well 1
-                   mapping = aes(x = date.,
-                                 y = well_1,
-                                 color = is_predicted)) +
-        geom_line(data = combined,   # well 2
-                  mapping = aes(x = date.,
-                                y = well_2,
-                  color = "grey70")) +
-        geom_line(data = combined,
-                  mapping = aes(x = date.,
-                                y = well_1)) +
-        scale_y_reverse() +
-        labs(x = "Date",
-             y = "Water Table Depth (cm)")
-      
-    }
-  })
-  
-  # table with water depth and predictions results
-  output$mytable = DT::renderDataTable({
+  Dataset <- reactive({
     
     Well_1_data <- Well_1_input()
     Well_2_data <- Well_2_input()
@@ -164,11 +121,112 @@ server <- function(input, output){
       mutate(is_predicted = ifelse(is.na(well_1), TRUE, FALSE))
     combined$well_1 <- na.approx(combined$well_1, na.rm = FALSE) #interpolate NAs
     
-    # print table
-    combined
+    return(combined)
   })
   
+  # Calculate and plot output
+  output$Plot <- renderPlot({
+    
+    well_data <- Dataset()
+      
+    if(input$Well_2_Plot == FALSE){
+      ggplot() +
+        geom_point(data = well_data,
+                   mapping = aes(x = date.,
+                                 y = well_1,
+                                 color = is_predicted)) +
+        geom_line(data = well_data,
+                  mapping = aes(x = date.,
+                                y = well_1)) +
+        scale_y_reverse() +
+        labs(x = "Date",
+             y = "Water Table Depth (cm)")
+    } else{  # plotting well 1 and well 2
+      
+      ggplot() +
+        geom_point(data = well_data,  # well 1
+                   mapping = aes(x = date.,
+                                 y = well_1,
+                                 color = is_predicted)) +
+        geom_line(data = well_data,   # well 2
+                  mapping = aes(x = date.,
+                                y = well_2,
+                  color = "grey70")) +
+        geom_line(data = well_data,
+                  mapping = aes(x = date.,
+                                y = well_1)) +
+        scale_y_reverse() +
+        labs(x = "Date",
+             y = "Water Table Depth (cm)")
+      
+    }
+  })
   
+  # table with water depth and predictions results
+  output$mytable = DT::renderDataTable({
+    
+    well_data <- Dataset()
+    
+    # print table
+    well_data
+  })
+  
+  output$downloadplot <- downloadHandler(
+    filename <- function(){
+      paste('plot', 'png', sep = ".")
+    },
+    content <- function(file){
+      png(file)
+      
+      well_data <- Dataset()
+      
+      plot <- ggplot()
+      
+      if(input$Well_2_Plot == FALSE){  # only plotting well 1
+        
+        plot <- ggplot() +
+          geom_point(data = well_data,
+                     mapping = aes(x = date.,
+                                   y = well_1,
+                                   color = is_predicted)) +
+          geom_line(data = well_data,
+                    mapping = aes(x = date.,
+                                  y = well_1)) +
+          scale_y_reverse() +
+          labs(x = "Date",
+               y = "Water Table Depth (cm)")
+      } else{  # plotting well 1 and well 2
+        
+        plot <- ggplot() +
+          geom_point(data = well_data,  # well 1
+                     mapping = aes(x = date.,
+                                   y = well_1,
+                                   color = is_predicted)) +
+          geom_line(data = well_data,   # well 2
+                    mapping = aes(x = date.,
+                                  y = well_2,
+                                  color = "grey70")) +
+          geom_line(data = well_data,
+                    mapping = aes(x = date.,
+                                  y = well_1)) +
+          scale_y_reverse() +
+          labs(x = "Date",
+               y = "Water Table Depth (cm)")
+      }
+        
+        print(plot)
+        dev.off()
+      
+    },
+    
+    contentType = "image/png"
+  )
+  
+  output$downloaddata <- downloadHandler(
+    filename = function() {"Well_1_Predicted_data.csv"}, content = function(file){
+      write.csv(Dataset(), file, row.names = FALSE)
+    }
+  )
  
 }
 
