229e1eb1999eba50e3ef9a0b764215e30da59173
ltri97 <46967722+ltri97@users.noreply.github.com>
2020-03-31 21:43:54 -0400

    Adding a version of the app that uses hourly data and the interpolation algorithm to fill in missing gaps

---
+++ Cluster_Group/myApp/app.R
@@ -0,0 +1,176 @@
+library(DT)
+library(shiny)
+library(tidyverse)
+library(lubridate)
+library(zoo)
+library(shinycssloaders)
+
+# import data
+setwd("D:/Capstone/data")
+wells <- read_csv("hourly.csv")
+clusters <- read_csv("dtw_result.csv")
+wellsList <- as.list(unique(wells$Well))
+
+ui <- fluidPage(
+  titlePanel("Predictive Well Data Gap Filling"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      h3("Well Selection"),
+      h5("Well 1 is the well containing missing data that will need to be predicted."),
+      selectInput("well1", label = h5("Select Well One:"),
+                  choices = wellsList),
+      uiOutput("Well_2_Req"),
+      uiOutput("date_req"),
+      checkboxInput("Well_2_Plot", "Check to view Well 2 plot data in grey", FALSE)
+    ),
+    
+    mainPanel(
+      plotOutput("Plot") %>% withSpinner(color = "#0dc5c1"),
+      DT::dataTableOutput("mytable") %>% withSpinner(color = "#0dc5c1")
+    )
+  )
+  
+)
+
+server <- function(input, output){
+  
+  Well_1_input <- reactive({
+    wells %>% 
+      filter(Well == input$well1) %>% 
+      as.data.frame()
+  })
+  
+  output$Well_2_Req <- renderUI({
+    
+    well_1_cluster <- clusters %>% 
+      filter(Well == input$well1) %>% 
+      select(Cluster) %>% 
+      as.numeric()
+    
+    well_2_options <- clusters %>% 
+      filter(Cluster == well_1_cluster) %>% 
+      as.list()
+    
+    selectInput("Well_2_Selection", label = h5("Select Well 2:"),
+                choices = well_2_options,
+                selected = 1)
+    
+  })
+  
+  Well_2_input <- reactive({
+    wells %>% 
+      filter(Well == input$Well_2_Selection) %>% 
+      as.data.frame()
+  })
+  
+  output$date_req <- renderUI({
+    
+    Well_1_data <- Well_1_input()
+    Well_2_data <- Well_2_input()
+    
+    min_date <- "2012-01-01"
+    max_date <- "2012-06-01"
+    
+    h4("Date Selection")
+    dateRangeInput("dates",
+                   label = h5("Select the date range for the missing data:"),
+                   min = min_date,
+                   max = max_date,
+                   start = min_date,
+                   end = max_date)
+    
+  })
+  
+  # Calculate and plot output
+  output$Plot <- renderPlot({
+    
+    Well_1_data <- Well_1_input()
+    Well_2_data <- Well_2_input()
+    
+    Well_1_data <- Well_1_data %>% 
+      filter(date. >= input$dates[1] & date. <= input$dates[2])
+    Well_2_data <- Well_2_data %>% 
+      filter(date. >= input$dates[1] & date. <= input$dates[2])
+    
+    Well_1_data <- Well_1_data %>% 
+      select(date., wtdepth)
+    Well_2_data <- Well_2_data %>% 
+      select(date., wtdepth)
+    
+    combined <- left_join(Well_2_data, Well_1_data, by = "date.")
+    colnames(combined) <- c("date.", "well_2", "well_1")
+    
+    combined <- combined %>% 
+      mutate(is_predicted = ifelse(is.na(well_1), TRUE, FALSE))
+    combined$well_1 <- na.approx(combined$well_1, na.rm = FALSE) #interpolate NAs
+
+    
+    if(input$Well_2_Plot == FALSE){  # only plotting well 1
+      
+      ggplot() +
+        geom_point(data = combined,
+                   mapping = aes(x = date.,
+                                 y = well_1,
+                                 color = is_predicted)) +
+        geom_line(data = combined,
+                  mapping = aes(x = date.,
+                                y = well_1)) +
+        scale_y_reverse() +
+        labs(x = "Date",
+             y = "Water Table Depth (cm)")
+    } else{  # plotting well 1 and well 2
+      
+      ggplot() +
+        geom_point(data = combined,  # well 1
+                   mapping = aes(x = date.,
+                                 y = well_1,
+                                 color = is_predicted)) +
+        geom_line(data = combined,   # well 2
+                  mapping = aes(x = date.,
+                                y = well_2,
+                  color = "grey70")) +
+        geom_line(data = combined,
+                  mapping = aes(x = date.,
+                                y = well_1)) +
+        scale_y_reverse() +
+        labs(x = "Date",
+             y = "Water Table Depth (cm)")
+      
+    }
+  })
+  
+  # table with water depth and predictions results
+  output$mytable = DT::renderDataTable({
+    
+    Well_1_data <- Well_1_input()
+    Well_2_data <- Well_2_input()
+    
+    Well_1_data <- Well_1_data %>% 
+      filter(date. >= input$dates[1] & date. <= input$dates[2])
+    Well_2_data <- Well_2_data %>% 
+      filter(date. >= input$dates[1] & date. <= input$dates[2])
+    
+    Well_1_data <- Well_1_data %>% 
+      select(date., wtdepth)
+    Well_2_data <- Well_2_data %>% 
+      select(date., wtdepth)
+    
+    combined <- left_join(Well_2_data, Well_1_data, by = "date.")
+    colnames(combined) <- c("date.", "well_2", "well_1")
+    
+    # interpolation of well 1 based on well 2 data
+    combined <- combined %>% 
+      mutate(is_predicted = ifelse(is.na(well_1), TRUE, FALSE))
+    combined$well_1 <- na.approx(combined$well_1, na.rm = FALSE) #interpolate NAs
+    
+    # print table
+    combined
+  })
+  
+  
+ 
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
--- Cluster_Group/testing_interpolation.R
+++ Cluster_Group/testing_interpolation.R
@@ -4,18 +4,6 @@ library(zoo)
 
 setwd("D:/Capstone/data")
 hourly <- read_csv("oneHourSummary.csv")
-hourly <- hourly %>% 
-  filter(Well == "K11",
-         date. <= "2013-01-01",
-         date. >= "2012-01-01") %>% 
-  select(date., level, wtdepth)
-
-colnames(hourly) <- c("date", "water_level", "water_depth")
-
-ggplot(hourly,
-       aes(x = date,
-           y = water_depth)) +
-  geom_point()
 
 #CREATING ARTIFICIAL GAP BETWEEN FEBRUARY AND MARCH
 hourly_gaps <- hourly %>% 
@@ -76,10 +64,29 @@ a6_gaps <- a6 %>%
 to_fill <- zoo(a6_gaps$wtdepth, a6_gaps$date.)  
 filler <- zoo(i7$wtdepth, i7$date.)
 combined <- merge(filler, to_fill) #combine data
+combined <- combined %>% 
+  transform(is_predicted = ifelse(is.na(to_fill), TRUE, FALSE))
 combined$to_fill <- na.approx(combined$to_fill, na.rm = TRUE) #interpolate NAs
-final_combination <- combined[index(filler),]
-final_combination <- fortify.zoo(final_combination, name = "date")
+final_combination <- fortify.zoo(combined, name = "date.") #changes zoo object back to a data frame while keeping a date column
+colnames(final_combination) <- c("date.", "Well_2", "Well_1", "is_predicted")
+final_combination <- final_combination %>% 
+  mutate(is_predicted = ifelse(is_predicted == 1, TRUE, FALSE))
 
+ggplot() +
+  geom_point(data = final_combination,
+            mapping = aes(x = date.,
+                          y = Well_1,
+                          color = is_predicted)) +
+  geom_line(data = final_combination,
+            mapping = aes(x = date.,
+                          y = Well_1)) +
+  geom_line(data = final_combination,
+            mapping = aes(x = date.,
+                          y = Well_2),
+                          color = "grey70") +
+  scale_color_manual(labels = c("Original", "Predicted"), values = c("red", "blue"), name = "values") +
+  scale_y_reverse()
+?labs
 ?na.approx
 ?index
 
@@ -98,13 +105,36 @@ a6_gaps <- a6_gaps %>%
 final_combination <- final_combination %>% 
   mutate(date = row())
 
+a6_gaps <- a6_gaps %>% 
+  select(date., wtdepth)
+i7 <- i7 %>% 
+  select(date., wtdepth)
+
+combined <- left_join(i7, a6_gaps, by = "date.")
+combined <- zoo(combined)
+colnames(combined) <- c("date.", "well_2", "well_1")
+head(combined)
+ncol(combined)
 a6_year_gap <- a6 %>% 
   mutate(wtdepth = ifelse(date. >= "2012-02-01", ifelse(date. <= "2013-01-01", NA, wtdepth), wtdepth))
+
+combined <- combined %>% 
+  transform(is_predicted = ifelse(is.na(well_1), TRUE, FALSE))
+
 
 
 to_fill <- zoo(a6_year_gap$wtdepth, a6_year_gap$date.)  
 filler <- zoo(i7$wtdepth, i7$date.)
 combined <- merge(filler, to_fill) #combine data
-combined$to_fill <- na.approx(combined$to_fill, na.rm = FALSE) #interpolate NAs
+combined$to_fill <- na.approx(combined$to_fill, na.rm = TRUE) #interpolate NAs
 final_combination <- combined[index(filler),]
-final_combination <- fortify.zoo(final_combination, name = "date")
+final_combination <- fortify.zoo(final_combination, name = "date.")
+?na.approx
+
+
+combined <- left_join(i7, a6_gaps, by = "date.")
+colnames(combined) <- c("date.", "well_2", "well_1")
+combined <- combined %>% 
+  mutate(is_predicted = ifelse(is.na(well_1), TRUE, FALSE))
+combined$well_1 <- na.approx(combined$well_1, na.rm = FALSE)
+str(combined)
