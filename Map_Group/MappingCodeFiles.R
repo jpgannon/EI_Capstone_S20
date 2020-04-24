c4183bef2e89528aad41a0b56a57260778a58af3
rmelton13 <60794938+rmelton13@users.noreply.github.com>
2020-04-24 15:29:37 -0400

    Update MappingCodeFiles.R

--- Map_Group/MappingCodeFiles.R
+++ Map_Group/MappingCodeFiles.R
@@ -4,21 +4,20 @@ library(raster)
 library(rgdal)
 
 
+
 # update raster package
 #install.packages('raster', repos = 'http://r-forge.r-project.org/', type = 'source')
 
 # Set working directory
-# setwd("~/VirginiaTech/Capstone/Working_Directory")
+ #setwd("~/VirginiaTech/Capstone/Working_Directory")
 
 # Load the txt file for mapping
-well_locations <- read_csv("Well_LocationsDD.txt")
+well_locations <- read_csv("Well_location_data.csv")
 
 
 # Load txt file and convert columns to characters for the labels
 well_labels <- well_locations %>%
-  mutate(OBJECTID = as.character(OBJECTID)) %>%
-  mutate(POINT_X = as.character(POINT_X)) %>%
-  mutate(POINT_Y = as.character(POINT_Y))
+  mutate(OBJECTID = as.character(OBJECTID)) 
 
 
 # read in TWI raster file 
@@ -33,9 +32,9 @@ twi <- raster('ws3cliptwid.tif')
  ws3streams <- readOGR("~/VirginiaTech/Capstone/Working_Directory/Shapefiles/ws3streams_proj.shp") %>%
                 spTransform(CRS("+proj=longlat +datum=WGS84 +no_defs"))
  plot(ws3streams)
-# ws3streams[ws3streams$StrType == E, "StrType"]
+ #ws3streams[ws3streams$StrType == E, "StrType"]
 # streams_fix <- ws3streams %>% 
-#   replace(as.character(StrType), "E", "Ephemeral")
+ #  replace(as.character(StrType), "E", "Ephemeral")
  
  
  #set legend/colors for streams
@@ -58,9 +57,7 @@ pal <- colorBin("Blues", domain = NULL, bins = 5, na.color = "transparent")
 pal2 <- colorNumeric(palette = "Blues", domain = vals)
 
 #Popup labels
-pop_ups <- c(well_labels$Well, 
-               well_labels$POINT_X, 
-               well_labels$POINT_Y)
+pop_ups <- well_raster_vals
 
 # read in slope
 ws3slope <- raster('~/VirginiaTech/Capstone/Working_Directory/Raster_files/ws3slope2')
@@ -78,6 +75,7 @@ vals <- values(na.omit(twi))
 slope_vals <- values(na.omit(ws3slope))
 uaa_vals <- values(na.omit(uaa))
 soil_vals <- values(na.omit(soil))
+#stream_vals <- values(na.omit(ws3streams))
   
   # Map; both rasters & shapefile with markers, legend, and layer control
 map <- leaflet(well_locations) %>%
@@ -87,13 +85,18 @@ map <- leaflet(well_locations) %>%
   addRasterImage(uaa, colors = uaa_pal, opacity = 0.5, group = "Upslope Accumulated Area") %>%
   addRasterImage(soil, colors = pal_soil, opacity = 0.5, group = "Soil") %>%
   addPolygons(data = ws3, color = "Black", fill = FALSE) %>%
-  addPolylines(data = ws3streams$StrType, fillColor = streams_col(ws3streams$StrType), group = "Streams") %>%
+  addPolylines(data = ws3streams, color = streams_col, group = "Streams") %>%
   addCircleMarkers(lng = well_locations$POINT_X, lat = well_locations$POINT_Y,
-             color = "Black", popup = paste("Well ID:", well_labels$Well, "X Coordinate:", well_labels$POINT_X)) %>%
+             color = "Black", popup = paste("Well ID:", well_locations$Well,
+                                             "Distance to Streams:", well_locations$DistanceToStreams, 
+                                            "Distance to Bedrock:", well_locations$DistanceToBedrock,
+                                            "TWI:", well_locations$TWI,
+                                             "Slope:", well_locations$Slope,
+                                            "UAA:", well_locations$UAA_Weighted)) %>%
   addLegend(position = 'topright', values = vals, pal = pal2, labFormat = labelFormat(),
             title = "Topographic <br> Wetness Index", group = "Topographic Wetness Index") %>%
   addLegend(position = 'topright', values = ws3streams$StrType, pal = streams_col, 
-            labFormat = labelFormat(c("Ephemeral", "Intermittent", "Perennial")), title = "Streams", group = "Streams") %>%
+           labFormat = labelFormat(c("Ephemeral", "Intermittent", "Perennial")), title = "Streams", group = "Streams") %>%
   addLegend(position = 'topright', values = slope_vals, group = "Slope", pal = pal_slope, labFormat = labelFormat(),
             title = "Slope") %>%
   addLegend(position = 'topright', values = uaa_vals, pal = uaa_pal, group = "Upslope Accumulated Area", title = "UAA") %>%
@@ -103,3 +106,4 @@ map <- leaflet(well_locations) %>%
                    options = layersControlOptions(collapsed = TRUE))%>%
   hideGroup(c("Topographic Wetness Index", "Slope", "Upslope Accumulated Area", "Soil", "Streams"))
 map
+
